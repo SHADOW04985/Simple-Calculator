9f1700a6ecf9c4046e4f3e167942612f3ad6269f
Aaryaman Maity <aaryamanmaity@gmail.com>
2022-07-11 18:17:45 +0530

    added features

--- script.js
+++ script.js
@@ -4,7 +4,20 @@ function inp(input) {
     var calc = document.getElementById("val").value;
     var last = calc.split('').pop();
     if (calc.length === 0 && input ==="/") {}
-    else if ((input === "+" || input === "-" || input === "*" || input === "/") && (last === "+" || last === "-" || last === "*" || last === "/")){}
+    else if (input === "." && (last === "." || last === "(" || last === ")")){}
+    else if (input === "(" && (last === "(" || last === ")" || last === ".")){}
+    else if (input === "(" && (last === "0" || last == "1" || last === "2" || last == "3" || last === "4" || last == "5" || last === "6" || last == "7" || last === "8" || last == "9" || last === ".")) {}
+    else if(input === ")" && calc.length === 0) {}
+    else if (input === ")" && (last === ")" || last === "(" || last === ".")){}
+    else if (input === ")" && (last === "+" || last === "-" || last === "*" || last === "/" || last === ".")){}
+    else if ((input === "+" || input === "-" || input === "*" || input === "/") && (last === "+" || last === "-" || last === "*" || last === "/")) {
+        var newchar = input.toString()
+        calc = calc.split('')
+        calc.pop()
+        calc.push(newchar)
+        calc = calc.join('')
+        document.getElementById("val").value = calc
+    }
     else {
         if (fval === 1 && input !== "+" && input !== "-" && input !== "*" && input !== "/") {
             fval = 0
@@ -38,4 +51,17 @@ function equate() {
     var calc = document.getElementById("val").value
     document.getElementById("val").value = eval(calc)
     fval = 1
+}
+
+function deleteval() {
+    var calc = document.getElementById("val").value
+    // var last = calc.split('').pop();
+    if (calc.length === 0){}
+    // else if (last === ',')
+    else {
+        calc = calc.split('')
+        calc.pop()
+        calc = calc.join('')
+        document.getElementById('val').value = calc
+    }
 }
